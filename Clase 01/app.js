717c83853411e26aeef5cca2832ea71780694728
nicolás cabrera <nicocabrera8@outlook.com>
2024-05-30 20:54:56 -0300

    repaso de javascript

---
+++ Clase 01/app.js
@@ -0,0 +1,12 @@
+
+
+
+let suma = (a,b)=>a+b
+
+
+
+let usuario = "Nicolas"
+
+let saludo = `Hola ${process.argv[2] === "frio"?usuario:"invitado"}`
+
+console.log(saludo);
--- Clase 02/app.js
+++
@@ -1,1 +0,0 @@
-console.log("Es solo una prueba");
