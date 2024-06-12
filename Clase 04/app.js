bce8e867f05d5284e2e193e6d2d4fe7d5e9d269e
Nicolas D. Cabrera PC escritorio <nicocabrera8@outlook.com>
2024-06-12 07:13:17 -0300

    agrego modificaciones en ejercicio de la clase 04

--- Clase 04/app.js
+++ Clase 04/app.js
@@ -1,4 +1,6 @@
-/* Obsoleto y no escalable
+/*               PROBLEMA 1 ----------------------------------Obsoleto y no escalable
+
+
 const numList = [];
 const diezNumeros = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 const respuesta = diezNumeros.map((num) => numList.push(Math.random()));
@@ -6,20 +8,24 @@ const respuesta = diezNumeros.map((num) => numList.push(Math.random()));
 
 //La siguiente es una mejor manera
 const elementosArray = 10;
-const diezNumeros = Array.from({ length: elementosArray }, () => Math.random());
+//crea un array dde cantidad igual a la definida en la variable elementosArray (cada elemento es un undefined)
+const diezNumeros = Array.from({ length: elementosArray }, () => {
+  return Math.random();
+});
 
-/*Obsoleto y no escalable
+/*                PROBLEMA 2 ----------------------------------Obsoleto y no escalable
+Obsoleto y no escalable
+const array = [1,1,2,2,2,2,3,4,5,6]
 const calcularRepetidos = (array) => {
   return array.reduce((previo, numero) => {
-    previo[numero] == numero
-      ? (previo[numero] = previo[numero] + 1)
-      : (previo[numero] = numero);
+    previo[numero] == numero? (previo[numero] = previo[numero] + 1): (previo[numero] = numero);
     return previo;
   }, {});
 };
 */
 
 //La siguiente es una mejor manera
+const array = [1, 1, 2, 2, 2, 2, 3, 4, 5, 6];
 const calcularRepetidos = (array) => {
   return array.reduce((previo, numero) => {
     return {
@@ -28,3 +34,4 @@ const calcularRepetidos = (array) => {
     };
   }, {});
 };
+console.log(calcularRepetidos(array));
