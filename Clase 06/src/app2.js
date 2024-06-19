446bc398c8dee74177823cf82b9b4074a3259a2c
nicocabrerafit2 <nicocabrera8@outlook.com>
2024-06-18 21:05:23 -0300

    termino de agregar todo lo visto en la clase

--- Clase 06/src/app2.js
+++ Clase 06/src/app2.js
@@ -1,9 +1,30 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Servidor creado con express");
+/*
+Metodo app.get mal ubicado (se tiene que colocar al final de todos los app.get):
+app.get("/:cualquierURL", (req, res) => {
+  res.status(404).send("No se encuentra esa url");
 });
+*/
+app.get("/user", (req, res) => {
+  const { name, edad } = req.query;
+  const baseDatos = [
+    { name: "nico", edad: 18 },
+    { name: "roberto", edad: 56 },
+  ];
+  //http://localhost:8080/user?name=roberto&edad=18
+  if (!name) {
+    res.send("No se introdujo el name en los query");
+  } else {
+    const userFinded = baseDatos.find((user) => user.name === name);
+    if (!userFinded) {
+      res.send("No se encontro ese nombre en la base de datos");
+    }
+    res.send(userFinded);
+  }
+});
+
 app.get("/:cualquierURL", (req, res) => {
   res.status(404).send("No se encuentra esa url");
 });
