417ace820441bac8c590db85e69e620113a08777
nicocabrerafit2 <nicocabrera8@outlook.com>
2024-07-16 07:08:33 -0300

    termino clase de after 1

--- Clase 05 1 after/app.js
+++ Clase 05 1 after/app.js
@@ -8,12 +8,12 @@ class TicketManager {
   getEvents() {
     return this.events;
   }
-  addEvent(nombre, lugar, precio, capacidad = 50, fecha = new Date()) {
+  addEvent({ nombre, lugar, precio, capacidad = 50, fecha = new Date() }) {
     const newEvent = {
       id: this.id++,
       nombre,
       lugar,
-      precio: precio * this.precioBaseDeGanancia,
+      precio: precio * this.#precioBaseDeGanancia,
       capacidad,
       fecha,
       participantes: [],
@@ -34,12 +34,29 @@ class TicketManager {
   }
 
   ponerEventoEnGira(eventId, lugar, fecha) {
-    const eventIndex = this.events.findIndex((event) => event.id === eventId);
+    const eventFinded = this.events.find((event) => event.id === eventId);
+    const newEvent = {
+      ...eventFinded,
+      id: this.id++,
+      lugar,
+      fecha,
+      participantes: [],
+    };
+    this.events.push(newEvent);
   }
 }
 
 const tiketera = new TicketManager();
 
-console.log(tiketera.addEvent());
-console.log(tiketera.addUser(1, 1));
-console.log(tiketera.getEvents());
+// const eventPrueba = {
+//   nombre: "arjona",
+//   lugar: "buenos aires",
+//   precio: 1000,
+//   capacidad: 600,
+//   fecha: "20204-04-29",
+// };
+
+// console.log(tiketera.addEvent(eventPrueba));
+// console.log(tiketera.addUser(1, 1));
+// console.log(tiketera.ponerEventoEnGira(1, "cordoba", "2028-01-21"));
+// console.log(tiketera.getEvents());
