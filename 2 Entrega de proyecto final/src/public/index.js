a752230e21518204fb84080f2eae17d1fa4b4652
Nicolas D. Cabrera PC escritorio <nicocabrera8@outlook.com>
2024-07-22 19:21:55 -0300

    mejoro el estilo de la vista productos con websocket

--- 2 Entrega de proyecto final/src/public/index.js
+++ 2 Entrega de proyecto final/src/public/index.js
@@ -5,8 +5,17 @@ socket.on("showProducts", (products) => {
   productContainer.innerHTML = "";
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = product.title;
-    productContainer.appendChild(li);
+    li.innerHTML = product.title
+    const price = document.createElement("p");
+    price.innerHTML = "Precio: "+product.price
+    const description = document.createElement("p");
+    description.innerHTML = "Descripcion: "+product.description
+    const category = document.createElement("p");
+    category.innerHTML = "Categoria: "+product.category
+    const div = document.createElement("div")
+    div.classList.add("producto");
+    div.append(li,description,category,price);
+    productContainer.appendChild(div);
   });
 });
 socket.on("error", (messaje) => {
